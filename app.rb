524f8e8a4dc55b5581dd4618c91fd64f8e11b418
Mika Tuupola <tuupola@appelsiini.net>
2010-11-11 16:52:08 +0200

    Configurable cache folders.

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require "haml"
 require "sinatra"
 require "pp"
 
-use Rack::Static, :urls => ["/css", "/js", "/images"], :root => "public"
+use Rack::Static, :urls => ["/css", "/js", "/img"], :root => "public"
+
+configure :development do
+  set :cuty_capt, "/Users/tuupola/bin/CutyCapt";
+end
+
+configure :production do
+  set :cuty_capt, "/home/tuupola/cutycapt/CutyCapt/xvfb-run.sh --server-args='-screen 0, 1024x768x24' /home/tuupola/cutycapt/CutyCapt/CutyCapt"
+end
+
+set :cache_folder, Proc.new { File.join(root, "public", "img") }
 
 before do
 end
@@ -18,10 +28,10 @@ post "/" do
 end
 
 get "/1.0/*" do
-  out = "/Users/tuupola/Code/ruby/shot/public/img/test.png"
-  system("/Users/tuupola/bin/CutyCapt --url='#{url}' --out='#{out}' --plugins=on --delay=1000")
+  out = File.join(settings.cache_folder, "test.png")
+  system("#{settings.cuty_capt} --url='#{url}' --out='#{out}' --plugins=on --delay=1000")
   send_file(out, :disposition => "inline", 
-                 #:filename => File.basename(out),
+  #               :filename => File.basename(out),
                  :type => "image/png")
 end
 
