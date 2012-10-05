da53af3c46426b4d698af9d5a82c8505e5bf3669
Mika Tuupola <tuupola@appelsiini.net>
2012-10-05 13:17:20 +0300

    Deploy rules for Bundler.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
-set :application, "screenshot"
+require "bundler/vlad"
+
+set :application, "screenshot2"
 #set :repository, "git@github.com:tuupola/#{application}.git"
 set :repository, "git@github.com:tuupola/screenshot.git"
 set :user, "sinatra"
@@ -12,23 +14,23 @@ require "vlad"
 
 namespace :vlad do
   desc "Deploy the code and restart the server"
-  task :deploy => [:update, :start_app]  
+  task :deploy => [:update, :"bundle:install", :start_app]  
 end
 
 namespace :dev do
   task :start_shotgun do
-    system "shotgun --port=#{local_port} rackup.ru"
+    system "bundle exec shotgun --port=#{local_port} config.ru"
   end
 
   desc "Start ssh tunnel between #{server}:#{remote_port} and localhost:#{local_port}"
   task :start_tunnel do
     puts "Tunneling  #{server}:#{remote_port} to localhost:#{local_port}"
-    system "autossh -M 48484 -nNT -g -R *:#{remote_port}:127.0.0.1:#{local_port} #{server}"
+    system "autossh -M 48485 -nNT -g -R *:#{remote_port}:127.0.0.1:#{local_port} #{server}"
   end
 
   remote_task :symlink do
-    puts "Symlinking shared/htaccess to current/public/.htaccess"
-    run "ln -f -s #{shared_path}/htaccess #{current_release}/public/.htaccess"
+    puts "Symlinking current/public/htaccess to current/public/.htaccess"
+    run "ln -f -s #{current_release}/public/htaccess #{current_release}/public/.htaccess"
   end
 
   desc "Switch to tunneled development mode."
